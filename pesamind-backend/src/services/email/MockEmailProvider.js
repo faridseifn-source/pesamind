f835e26450c0eff00554463d545607e968b3557d
faridseifn-source <faridseifn@googlemail.com>
2026-08-03 18:49:02 +0300

    Delete pesamind-backend directory

--- pesamind-backend/src/services/email/MockEmailProvider.js
+++
@@ -1,9 +0,0 @@
-const { EmailProvider } = require("./EmailProvider");
-
-class MockEmailProvider extends EmailProvider {
-  async sendPasswordReset(toEmail, code) {
-    console.log(`[email:mock] To: ${toEmail} | Subject: Your PesaMind password reset code | Code: ${code} (expires in 30 min)`); // eslint-disable-line no-console
-  }
-}
-
-module.exports = { MockEmailProvider };
