91f7fdec19d4c22884431fe6ffe270e66b48c5db
faridseifn-source <faridseifn@googlemail.com>
2026-08-05 19:00:13 +0300

    Delete pesamind-backend directory

--- pesamind-backend/src/index.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-const env = require("./lib/env");
-
-app.listen(env.port, () => {
-  console.log(`PesaMind API listening on :${env.port} [${env.nodeEnv}]`); // eslint-disable-line no-console
-  console.log(
-    `Providers -> kyc:${env.providers.kyc} rail:${env.providers.paymentsRail} funding:${env.providers.cardFunding} issuing:${env.providers.cardIssuing}`
-  ); // eslint-disable-line no-console
-});
