8089ff09d4d591e36305d61cc36e53f6b8403c47
faridseifn-source <faridseifn@googlemail.com>
2026-08-07 19:18:25 +0300

    Delete pesamind-backend directory

--- pesamind-backend/src/services/sms/MockSmsProvider.js
+++
@@ -1,9 +0,0 @@
-const { SmsProvider } = require("./SmsProvider");
-
-class MockSmsProvider extends SmsProvider {
-  async sendOtp(phoneE164, code) {
-    console.log(`[sms:mock] To: ${phoneE164} | Your PesaMind verification code is ${code} (expires in 10 min)`); // eslint-disable-line no-console
-  }
-}
-
-module.exports = { MockSmsProvider };
