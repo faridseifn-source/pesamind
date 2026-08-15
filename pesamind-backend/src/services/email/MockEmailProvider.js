43f31a11908264bdbe282416672d0eb9d3fb647b
faridseifn-source <faridseifn@googlemail.com>
2026-08-16 00:47:41 +0300

    Add files via upload

---
+++ pesamind-backend/src/services/email/MockEmailProvider.js
@@ -0,0 +1,17 @@
+const { EmailProvider } = require("./EmailProvider");
+
+class MockEmailProvider extends EmailProvider {
+  async sendPasswordReset(toEmail, code) {
+    console.log(`[email:mock] To: ${toEmail} | Subject: Your PesaMind password reset code | Code: ${code} (expires in 30 min)`); // eslint-disable-line no-console
+  }
+
+  async sendAdminLoginCode(toEmail, code) {
+    console.log(`[email:mock] To: ${toEmail} | Subject: Your PesaMind admin login code | Code: ${code} (expires in 10 min)`); // eslint-disable-line no-console
+  }
+
+  async sendVerificationCode(toEmail, code) {
+    console.log(`[email:mock] To: ${toEmail} | Subject: Your PesaMind verification code | Code: ${code} (expires in 30 min)`); // eslint-disable-line no-console
+  }
+}
+
+module.exports = { MockEmailProvider };
